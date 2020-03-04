e9999236741825f88d2d0863943c2ad2671651d2
洪坤越 <hongkunyue.123@gmail.com>
2020-03-04 09:37:41 +0800

    webpack 配置使用

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,3 @@
-// // const path = require('path');
-// // const debug = process.env.NODE_ENV !== 'production';
-
-// // vue.config.js
-// module.exports = {
-//   publicPath: './',
-//   lintOnSave: false,
-//   devServer: {
-//     host: '0.0.0.0',
-//     port: 8080,
-//     // port: 8888, // 服务器临时部署端口
-//     proxy: {
-//       '/api': {
-//         target: 'http://localhost:3000',
-//         changeOrigin: true,
-//       }
-//     }
-//   }
-// };
 const path = require('path')
 const fs = require('fs')
 const webpack = require('webpack')
